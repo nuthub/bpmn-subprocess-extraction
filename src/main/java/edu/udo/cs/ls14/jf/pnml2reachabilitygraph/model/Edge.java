a274c499c0e0fedd3c947639f5a2c5f5e14cc7b3
Julian Flake <julian.flake@cs.tu-dortmund.de>
2014-09-02 13:16:34 +0200

    removed getSrc() and getTgt() since jung graph defines these methods in Graph

--- src/main/java/edu/udo/cs/ls14/jf/pnml2reachabilitygraph/model/Edge.java
+++ src/main/java/edu/udo/cs/ls14/jf/pnml2reachabilitygraph/model/Edge.java
@@ -2,35 +2,15 @@ package edu.udo.cs.ls14.jf.pnml2reachabilitygraph.model;
 
 public class Edge {
 
-	private Marking src;
 	private String t;
-	private Marking tgt;
 
 	public Edge() {
 		super();
 	}
-	
-	@Deprecated
-	public Edge(Marking src, String t, Marking tgt) {
-		super();
-		this.src = src;
-		this.t = t;
-		this.tgt = tgt;
-	}
-	
+
 	public Edge(String t) {
 		super();
 		this.t = t;
-	}
-	
-	@Deprecated
-	public Marking getSrc() {
-		return src;
-	}
-
-	@Deprecated
-	public void setSrc(Marking src) {
-		this.src = src;
 	}
 
 	public String getT() {
@@ -41,19 +21,9 @@ public class Edge {
 		this.t = t;
 	}
 
-	@Deprecated
-	public Marking getTgt() {
-		return tgt;
-	}
-
-	@Deprecated
-	public void setTgt(Marking tgt) {
-		this.tgt = tgt;
-	}
-
 	@Override
 	public String toString() {
-		return "Edge [" + src + ", " + t + ", " + tgt + "]";
+		return "Edge [" + t + "]";
 	}
 
 }
