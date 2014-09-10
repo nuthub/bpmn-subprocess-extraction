eba15dc030390ea596a50d0183f980cc2a9cf636
Julian Flake <julian.flake@cs.tu-dortmund.de>
2014-09-10 14:21:44 +0200

    added assertions regarding element counts

--- bpmn2bp/src/test/java/edu/udo/cs/ls14/jf/bpmn2pnml/test/Bpmn2PnmlConverterTest.java
+++ bpmn2bp/src/test/java/edu/udo/cs/ls14/jf/bpmn2pnml/test/Bpmn2PnmlConverterTest.java
@@ -10,6 +10,8 @@ import org.junit.Test;
 
 import edu.udo.cs.ls14.jf.bpmn2pnml.Bpmn2PnmlConverter;
 import edu.udo.cs.ls14.jf.bpmn2pnml.ProcessLoader;
+import fr.lip6.move.pnml.ptnet.hlapi.PageHLAPI;
+import fr.lip6.move.pnml.ptnet.hlapi.PetriNetHLAPI;
 
 public class Bpmn2PnmlConverterTest {
 
@@ -22,30 +24,63 @@ public class Bpmn2PnmlConverterTest {
 
 	@Test
 	public void testLoopTransformation() throws Exception {
-		runConversion("looping-events-example");
+		PetriNetHLAPI net = runConversionToPTNet("looping-events-example");
+		assertEquals(1, net.getPagesHLAPI().size());
+		PageHLAPI page = net.getPagesHLAPI().get(0);
+		assertEquals(9, page.getObjects_PlaceHLAPI().size());
+		assertEquals(9, page.getObjects_TransitionHLAPI().size());
+		assertEquals(18, page.getObjects_ArcHLAPI().size());
 	}
 
 	@Test
 	public void testXorWEventsTransformation() throws Exception {
-		runConversion("xor-example");
+		PetriNetHLAPI net = runConversionToPTNet("xor-example");
+		assertEquals(1, net.getPagesHLAPI().size());
+		PageHLAPI page = net.getPagesHLAPI().get(0);
+		assertEquals(8, page.getObjects_PlaceHLAPI().size());
+		assertEquals(8, page.getObjects_TransitionHLAPI().size());
+		assertEquals(16, page.getObjects_ArcHLAPI().size());
 	}
-	
+
 	@Test
 	public void testLoopingXorTransformation() throws Exception {
-		runConversion("looping-xor");
+		PetriNetHLAPI net = runConversionToPTNet("looping-xor");
+		assertEquals(1, net.getPagesHLAPI().size());
+		PageHLAPI page = net.getPagesHLAPI().get(0);
+		assertEquals(12, page.getObjects_PlaceHLAPI().size());
+		assertEquals(13, page.getObjects_TransitionHLAPI().size());
+		assertEquals(26, page.getObjects_ArcHLAPI().size());
 	}
 
 	@Test
 	public void testPM1Transformation() throws Exception {
-		runConversion("PM1-mit-Fragment1");
+		PetriNetHLAPI net = runConversionToPTNet("PM1-mit-Fragment1");
+		assertEquals(1, net.getPagesHLAPI().size());
+		PageHLAPI page = net.getPagesHLAPI().get(0);
+		assertEquals(12, page.getObjects_PlaceHLAPI().size());
+		assertEquals(9, page.getObjects_TransitionHLAPI().size());
+		assertEquals(22, page.getObjects_ArcHLAPI().size());
 	}
 
 	@Test
 	public void testPM2Transformation() throws Exception {
-		runConversion("PM2-mit-Fragment1");
+		PetriNetHLAPI net = runConversionToPTNet("PM2-mit-Fragment1");
+		assertEquals(1, net.getPagesHLAPI().size());
+		PageHLAPI page = net.getPagesHLAPI().get(0);
+		assertEquals(14, page.getObjects_PlaceHLAPI().size());
+		assertEquals(11, page.getObjects_TransitionHLAPI().size());
+		assertEquals(26, page.getObjects_ArcHLAPI().size());
 	}
 
-	private void runConversion(String basename) throws Exception {
+	private PetriNetHLAPI runConversionToPTNet(String basename)
+			throws Exception {
+		URL url = getClass().getResource("../../bpmn/" + basename + ".bpmn");
+		assertNotNull(url);
+		Process process = ProcessLoader.loadFirstProcessFromResource(url);
+		return converter.convertToPTNet(process);
+	}
+
+	private void runConversionToFile(String basename) throws Exception {
 		URL url = getClass().getResource("../../bpmn/" + basename + ".bpmn");
 		assertNotNull(url);
 		Process process = ProcessLoader.loadFirstProcessFromResource(url);
