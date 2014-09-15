abd58ab0c97b0aa7440a040407941b5fb60fedc1
Julian Flake <julian.flake@cs.tu-dortmund.de>
2014-09-15 15:25:51 +0200

    added tests for event based gateways

--- bpmn2bp/src/test/java/edu/udo/cs/ls14/jf/reachabilitygraph/test/RGGeneratorTest.java
+++ bpmn2bp/src/test/java/edu/udo/cs/ls14/jf/reachabilitygraph/test/RGGeneratorTest.java
@@ -1,18 +1,20 @@
-package edu.udo.cs.ls14.jf.pnml2reachabilitygraph.test;
+package edu.udo.cs.ls14.jf.reachabilitygraph.test;
 
 import java.io.File;
-import java.util.HashSet;
 import java.util.Set;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import org.junit.Test;
 
+import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
 import edu.udo.cs.ls14.jf.behaviorprofile.BehavioralProfile;
-import edu.udo.cs.ls14.jf.behaviorprofile.Matrix;
-import edu.udo.cs.ls14.jf.pnml2reachabilitygraph.ReachabilityGraph;
-import edu.udo.cs.ls14.jf.pnml2reachabilitygraph.ReachabilityGraphVisualizer;
+import edu.udo.cs.ls14.jf.reachabilitygraph.Edge;
+import edu.udo.cs.ls14.jf.reachabilitygraph.Marking;
+import edu.udo.cs.ls14.jf.reachabilitygraph.ReachabilityGraph;
 import edu.udo.cs.ls14.jf.reachabilitygraph.Trace;
+import edu.udo.cs.ls14.jf.utils.graph.GraphVisualizer;
 
 public class RGGeneratorTest {
 
@@ -51,6 +53,20 @@ public class RGGeneratorTest {
 		testGenerateProfile(traces);
 	}
 
+	@Test
+	public void eventBasedGatewayExclusive() throws Exception {
+		ReachabilityGraph g = testGenerateRG("event-based-gateway-exclusive");
+		Set<Trace> traces = g.getTraces();
+		testGenerateProfile(traces);
+	}
+
+	@Test
+	public void eventBasedGatewayParallel() throws Exception {
+		ReachabilityGraph g = testGenerateRG("event-based-gateway-parallel");
+		Set<Trace> traces = g.getTraces();
+		testGenerateProfile(traces);
+	}
+
 	private void testGenerateProfile(Set<Trace> traces) throws Exception {
 		BehavioralProfile bp = new BehavioralProfile();
 		bp.generateFromTraces(traces);
@@ -74,9 +90,15 @@ public class RGGeneratorTest {
 
 		ReachabilityGraph graph = rgtest.testGenerateRG("looping-xor");
 
-		JFrame frame = new ReachabilityGraphVisualizer().visualize(graph);
+		GraphVisualizer<Marking, Edge> vis = new GraphVisualizer<Marking, Edge>();
+		JPanel panel = vis.visualize(graph,
+				new ToStringLabeller<Marking>(),
+				new ToStringLabeller<Edge>());
+		// Create Window / App / JFrame
+		JFrame frame = new JFrame("Reachability Graph");
+		frame.getContentPane().add(panel);
+		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		// System.exit(0);
 	}
 }
