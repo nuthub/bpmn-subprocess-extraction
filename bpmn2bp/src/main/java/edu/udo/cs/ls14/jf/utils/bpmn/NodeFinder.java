18d1ba5366db74e4ccfde58faf645896ac2a5006
Julian Flake <julian.flake@cs.tu-dortmund.de>
2014-09-22 19:55:25 +0200

    static node finder

---
+++ bpmn2bp/src/main/java/edu/udo/cs/ls14/jf/utils/bpmn/NodeFinder.java
@@ -0,0 +1,39 @@
+package edu.udo.cs.ls14.jf.utils.bpmn;
+
+import org.eclipse.bpmn2.EndEvent;
+import org.eclipse.bpmn2.FlowElement;
+import org.eclipse.bpmn2.Process;
+import org.eclipse.bpmn2.StartEvent;
+
+/**
+ * TODO: break down to graph notion
+ * 
+ * @author flake
+ *
+ */
+public class NodeFinder {
+
+	/*
+	 * TODO: check for multiple start nodes
+	 */
+	public static StartEvent getStartNode(Process process) {
+		for (FlowElement elem : process.getFlowElements()) {
+			if (elem instanceof StartEvent) {
+				return (StartEvent) elem;
+			}
+		}
+		return null;
+	}
+
+	/*
+	 * TODO: check for multiple end nodes
+	 */
+	public static EndEvent getEndNode(Process process) {
+		for (FlowElement elem : process.getFlowElements()) {
+			if (elem instanceof EndEvent) {
+				return (EndEvent) elem;
+			}
+		}
+		return null;
+	}
+}
