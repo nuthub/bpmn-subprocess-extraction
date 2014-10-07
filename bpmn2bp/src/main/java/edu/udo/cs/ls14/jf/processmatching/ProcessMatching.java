9d36cd7aea8b9b874707a006d3a48764f5d4a76f
Julian Flake <julian.flake@cs.tu-dortmund.de>
2014-10-07 14:27:50 +0200

    added (not so deep) clone function

--- bpmn2bp/src/main/java/edu/udo/cs/ls14/jf/processmatching/ProcessMatching.java
+++ bpmn2bp/src/main/java/edu/udo/cs/ls14/jf/processmatching/ProcessMatching.java
@@ -1,5 +1,6 @@
 package edu.udo.cs.ls14.jf.processmatching;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.eclipse.bpmn2.FlowNode;
@@ -34,16 +35,29 @@ public class ProcessMatching {
 		return nodeCorrespondences;
 	}
 
-	public void setNodeCorrespondences(Set<Pair<FlowNode, FlowNode>> nodeCorrespondences) {
+	public void setNodeCorrespondences(
+			Set<Pair<FlowNode, FlowNode>> nodeCorrespondences) {
 		this.nodeCorrespondences = nodeCorrespondences;
 	}
 
-	public void setFragmentCorrespondences(Set<Pair<Fragment, Fragment>> fragmentCorrespondences) {
+	public void setFragmentCorrespondences(
+			Set<Pair<Fragment, Fragment>> fragmentCorrespondences) {
 		this.fragmentCorrespondences = fragmentCorrespondences;
 	}
 
 	public Set<Pair<Fragment, Fragment>> getFragmentCorrespondences() {
 		return fragmentCorrespondences;
+	}
+
+	public ProcessMatching clone() {
+		ProcessMatching clone = new ProcessMatching();
+		clone.setProcess1(process1);
+		clone.setProcess2(process2);
+		clone.setNodeCorrespondences(new HashSet<Pair<FlowNode, FlowNode>>(
+				nodeCorrespondences));
+		clone.setFragmentCorrespondences(new HashSet<Pair<Fragment, Fragment>>(
+				fragmentCorrespondences));
+		return clone;
 	}
 
 	public String toString() {
@@ -56,7 +70,7 @@ public class ProcessMatching {
 		sb.append(System.getProperty("line.separator"));
 		sb.append(" nodeCorrespondences=" + nodeCorrespondences + ",");
 		sb.append(System.getProperty("line.separator"));
-		sb.append(" fragmentCorrespondences=" + fragmentCorrespondences + ",");
+		sb.append(" fragmentCorrespondences=" + fragmentCorrespondences);
 		sb.append(System.getProperty("line.separator"));
 		sb.append("]");
 		sb.append(System.getProperty("line.separator"));
