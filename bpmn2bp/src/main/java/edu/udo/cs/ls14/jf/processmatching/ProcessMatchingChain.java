517b841016ced0825b633bf0815f168efa6bb8f1
Julian Flake <julian.flake@cs.tu-dortmund.de>
2014-10-22 11:59:38 +0200

    switched logging to slf4j

--- bpmn2bp/src/main/java/edu/udo/cs/ls14/jf/processmatching/ProcessMatchingChain.java
+++ bpmn2bp/src/main/java/edu/udo/cs/ls14/jf/processmatching/ProcessMatchingChain.java
@@ -1,51 +1,50 @@
 package edu.udo.cs.ls14.jf.processmatching;
 
 import org.eclipse.emf.ecore.resource.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ProcessMatchingChain {
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(ProcessMatchingChain.class);
 
 	public static ProcessMatching getCandidates(Resource res1, Resource res2)
 			throws Exception {
 
-		
 		// Find all matches
-		System.out.println("Find all matches.");
+		LOG.info("Find all matches.");
 		ProcessMatching allMatching = ProcessMatcher.match(res1, res2);
-		System.out.println(allMatching.getFragmentCorrespondences().size()
-				+ " fragment correspondences.");
-		
+		LOG.info(allMatching.getFragmentCorrespondences().size()
+				+ " fragment correspondences left.");
+
 		// Filter out pairs with non-matching conditions
-		System.out
-				.println("Filter out matches with inequivalent conditional profiles");
+		LOG.info("Filter out matches with inequivalent conditional profiles");
 		ProcessMatching conditionEquivalentMatching = InequivalentConditionsFCFilter
 				.filter(allMatching);
-		System.out.println(conditionEquivalentMatching
-				.getFragmentCorrespondences().size()
-				+ " fragment correspondences.");
-		
+		LOG.info(conditionEquivalentMatching.getFragmentCorrespondences()
+				.size() + " fragment correspondences left.");
+
 		// Filter out nested fragment matches
-		System.out.println("Filter out nested matches.");
+		LOG.info("Filter out nested matches.");
 		ProcessMatching filteredMatching = NestedFCFilter
 				.filter(conditionEquivalentMatching);
-		System.out.println(filteredMatching
-				.getFragmentCorrespondences().size()
-				+ " fragment correspondences.");
-		
+		LOG.info(filteredMatching.getFragmentCorrespondences().size()
+				+ " fragment correspondences left.");
+
 		// build sequence unions
-		System.out.println("Union matches in sequence.");
+		LOG.info("Union matches in sequence.");
 		ProcessMatching unionedMatching = SequentialFCJointer
 				.joinSequences(filteredMatching);
-		System.out.println(unionedMatching
-				.getFragmentCorrespondences().size()
-				+ " fragment correspondences.");
-		
+		LOG.info(unionedMatching.getFragmentCorrespondences().size()
+				+ " fragment correspondences left.");
+
 		// filter out trivial fragments (|nodes| < 2)
-		System.out.println("Filter out trivial matches.");
+		LOG.info("Filter out trivial matches.");
 		ProcessMatching candidates = TrivialFCFilter.filter(unionedMatching);
-		System.out.println(candidates
-				.getFragmentCorrespondences().size()
-				+ " fragment correspondences.");
-		
+		LOG.info(candidates.getFragmentCorrespondences().size()
+				+ " fragment correspondences left.");
+
 		return candidates;
 
 	}
