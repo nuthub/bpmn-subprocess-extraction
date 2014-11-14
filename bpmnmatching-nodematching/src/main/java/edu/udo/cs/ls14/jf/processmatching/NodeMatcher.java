29bc4b384eb3a4aa9e4c63991effd8a5416bbd7a
Julian Flake <julian.flake@cs.tu-dortmund.de>
2014-11-14 14:11:45 +0100

    nodematcher artifact

--- bpmnmatching-nodematching/src/main/java/edu/udo/cs/ls14/jf/processmatching/NodeMatcher.java
+++ bpmnmatching-nodematching/src/main/java/edu/udo/cs/ls14/jf/processmatching/NodeMatcher.java
@@ -1,12 +1,8 @@
 package edu.udo.cs.ls14.jf.processmatching;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.eclipse.bpmn2.FlowElement;
 import org.eclipse.bpmn2.FlowNode;
 import org.eclipse.bpmn2.Process;
-import org.javatuples.Pair;
 
 import edu.udo.cs.ls14.jf.bpmnmatching.BpmnMatchingFactory;
 import edu.udo.cs.ls14.jf.bpmnmatching.NodeMatching;
@@ -36,25 +32,6 @@ public class NodeMatcher {
 		}
 		return matching;
 
-	}
-
-	@Deprecated
-	public static Set<Pair<FlowNode, FlowNode>> getCorrespondencesOld(
-			Process process1, Process process2) throws Exception {
-		Set<Pair<FlowNode, FlowNode>> mapping = new HashSet<Pair<FlowNode, FlowNode>>();
-		NodeComparator comp = new NodeComparator();
-		for (FlowElement e1 : process1.getFlowElements()) {
-			if (e1 instanceof FlowNode) {
-				for (FlowElement e2 : process2.getFlowElements()) {
-					if (e2 instanceof FlowNode) {
-						if (comp.isEquivalent((FlowNode) e1, (FlowNode) e2)) {
-							mapping.add(Pair.with((FlowNode) e1, (FlowNode) e2));
-						}
-					}
-				}
-			}
-		}
-		return mapping;
 	}
 
 }
