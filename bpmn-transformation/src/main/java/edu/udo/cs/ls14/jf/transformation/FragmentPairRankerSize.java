4b5abd8610d01550b7d04b194e4c72e0f0e462ca
Julian Flake <julian.flake@cs.tu-dortmund.de>
2015-01-19 18:18:10 +0100

    Logging

--- bpmn-transformation/src/main/java/edu/udo/cs/ls14/jf/transformation/FragmentPairRankerSize.java
+++ bpmn-transformation/src/main/java/edu/udo/cs/ls14/jf/transformation/FragmentPairRankerSize.java
@@ -6,6 +6,8 @@ import org.eclipse.bpmn2.Activity;
 import org.eclipse.bpmn2.Event;
 import org.eclipse.bpmn2.FlowElement;
 import org.eclipse.bpmn2.Gateway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import edu.udo.cs.ls14.jf.bpmn.utils.FragmentUtil;
 import edu.udo.cs.ls14.jf.bpmnmatching.FragmentMatching;
@@ -13,15 +15,26 @@ import edu.udo.cs.ls14.jf.bpmnmatching.FragmentPair;
 
 public class FragmentPairRankerSize {
 
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(FragmentPairRankerSize.class);
+
 	public static FragmentMatching rankFragments(FragmentMatching matching) {
-		for(FragmentPair pair : matching.getPairs()) {
+		for (FragmentPair pair : matching.getPairs()) {
 			Predicate<FlowElement> filter = e -> e instanceof Activity
 					|| e instanceof Event || e instanceof Gateway;
-			int sizeA = FragmentUtil.getFlowElements(pair.getA(), filter).size();
-			int sizeB = FragmentUtil.getFlowElements(pair.getB(), filter).size();
+			int sizeA = FragmentUtil.getFlowElements(pair.getA(), filter)
+					.size();
+			int sizeB = FragmentUtil.getFlowElements(pair.getB(), filter)
+					.size();
 			if (sizeA <= sizeB) {
+				LOGGER.info("a is better Fragment in pair "
+						+ FragmentUtil.toString(pair.getA()) + " / "
+						+ FragmentUtil.toString(pair.getB()));
 				pair.setBetter(pair.getA());
 			} else {
+				LOGGER.info("b is better Fragment in pair "
+						+ FragmentUtil.toString(pair.getA()) + " / "
+						+ FragmentUtil.toString(pair.getB()));
 				pair.setBetter(pair.getB());
 			}
 		}
