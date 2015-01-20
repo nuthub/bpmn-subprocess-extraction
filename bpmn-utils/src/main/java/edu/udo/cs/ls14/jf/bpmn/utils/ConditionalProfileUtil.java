006f1a319fccda57c853656a9558575148ff2e7d
Julian Flake <julian.flake@cs.tu-dortmund.de>
2015-01-20 19:34:24 +0100

    conditional profile to tex

---
+++ bpmn-utils/src/main/java/edu/udo/cs/ls14/jf/bpmn/utils/ConditionalProfileUtil.java
@@ -0,0 +1,63 @@
+package edu.udo.cs.ls14.jf.bpmn.utils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.bpmn2.FlowNode;
+import org.eclipse.bpmn2.FormalExpression;
+
+import edu.udo.cs.ls14.jf.bpmnanalysis.ConditionalProfile;
+
+public class ConditionalProfileUtil {
+
+	private static final String NL = System.getProperty("line.separator");
+
+	public static String toTex(ConditionalProfile cp, List<String> nodes) {
+		Map<String, String> conditions = new HashMap<String, String>();
+		for (FlowNode key : cp.getRelations().keySet()) {
+			List<String> expressions = new ArrayList<String>();
+			for (FormalExpression exp : cp.getRelations().get(key)) {
+				expressions.add(exp.getBody());
+			}
+			Collections.sort(expressions);
+			String c = "";
+			c += StringUtils.join(expressions, ",");
+			conditions.put(key.getName(), c);
+		}
+
+		List<String> tuples = new ArrayList<String>();
+		for (String name : nodes) {
+			String c = " (";
+			c += "\\texttt{" + name + "}";
+			c += ", ";
+			if (!conditions.containsKey(name) || conditions.get(name).equals("")) {
+				c += "\\emptyset ";
+			} else {
+				c += "\\{" + conditions.get(name) + "\\}";
+			}
+			c += ")";
+			tuples.add(c);
+		}
+
+		StringBuffer sb = new StringBuffer();
+		sb.append("$CP_M = \\{" + NL);
+		sb.append(StringUtils.join(tuples, ", " + NL));
+		sb.append(NL + "\\}$");
+
+		System.out.println(sb.toString());
+		return sb.toString();
+
+	}
+
+	public static void writeDebugFiles(String debugFilesDir, String basename,
+			List<String> nodes, ConditionalProfile cp) throws IOException {
+		IOUtil.writeTxtFile(toTex(cp, nodes), debugFilesDir + basename
+				+ "-conditionalprofile.tex");
+
+	}
+}
