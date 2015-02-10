0695668ea38640f36b4a5d050a162bfab73132b7
Julian Flake <julian.flake@cs.tu-dortmund.de>
2015-02-10 12:27:31 +0100

    JavaDoc model

--- bpmn-subprocess-extraction-model/src/main/java/edu/udo/cs/ls14/jf/bpmnanalysis/util/NonUniqueList.java
+++ bpmn-subprocess-extraction-model/src/main/java/edu/udo/cs/ls14/jf/bpmnanalysis/util/NonUniqueList.java
@@ -3,19 +3,35 @@ package edu.udo.cs.ls14.jf.bpmnanalysis.util;
 import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.util.EObjectEList;
 
+/**
+ * Returns false for isUnique() calls. Also see
+ * https://bugs.eclipse.org/bugs/show_bug.cgi?id=89325. With this list type it
+ * is possible to create 0..* associations which contain a single object
+ * multiple times.
+ * 
+ * @author Julian Flake
+ *
+ * @param <E>
+ *            the associated type
+ */
 public class NonUniqueList<E> extends EObjectEList<E> {
 
 	/**
-	 * https://bugs.eclipse.org/bugs/show_bug.cgi?id=89325
+	 * generaed UID
 	 */
-	
 	private static final long serialVersionUID = 5365083419747685568L;
 
-	public NonUniqueList(Class<?> dataClass, InternalEObject owner, int featureID) {
+	/**
+	 * {@inheritDoc}
+	 */
+	public NonUniqueList(Class<?> dataClass, InternalEObject owner,
+			int featureID) {
 		super(dataClass, owner, featureID);
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	protected boolean isUnique() {
 		return false;
